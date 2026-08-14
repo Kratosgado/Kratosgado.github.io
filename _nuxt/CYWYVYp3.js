0452cb5171dc0c91d6d750c13b8f1c008781cb07
Kratosgado <Kratosgado@users.noreply.github.com>
2026-08-14 16:23:38 +0000

    fix: what again Kratosgado/portfolio@291fb688c3da3d161ddc83cd4a8aa9a22f3afd9c

---
+++ _nuxt/CYWYVYp3.js
@@ -0,0 +1,1 @@
+import{t as s}from"./_iPIppFg.js";import{aB as n,D as l,E as c,ag as p,W as u,a3 as m}from"./BOSbsnfW.js";const i={base:""},_={__name:"ProseStrong",props:{class:{type:null,required:!1}},setup(e){const a=e,o=n(),t=m(()=>s({extend:s(i),...o.ui?.prose?.strong||{}}));return(r,f)=>(l(),c("strong",{class:u(t.value({class:a.class}))},[p(r.$slots,"default")],2))}};export{_ as default};
